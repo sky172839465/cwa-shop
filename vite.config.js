edac35981b5862f070e76d061ecc6201ef788345
YuSong Hsu <yusong.hsu@yahooinc.com>
2025-11-21 19:07:14 +0800

    feat: using vite tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 import { viteMockServe } from 'vite-plugin-mock'
 import { analyzer } from 'vite-bundle-analyzer'
 import { sync } from 'glob'
@@ -77,6 +78,7 @@ export default ({ mode }) => {
     },
     root: `${entriesDir}/${isDefaultEntry ? '' : `/${ENTRY}`}`,
     plugins: [
+      tailwindcss(),
       react(),
       viteMockServe({
         mockPath: './src/mock',
